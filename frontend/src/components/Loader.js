2d31b7bb31fa9ffcb8d5abbb483489d3de439eea
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-20 16:22:54 +0100

    Fix prop name in Loader component

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,9 +1,9 @@
 const Loader = () => {
     console.log("Loading..........................................")
     return (
-        <div class="loader-container">
-            <div class="loader">
-            <div class="lds-grid">
+        <div className="loader-container">
+            <div className="loader">
+            <div className="lds-grid">
                 <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
             </div>
             <p>Loading...</p>
