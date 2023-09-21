9e9e85e93d04d0df84a32d2b214fdf00b13bed4f
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-21 16:59:07 +0100

    Change DEBUG settings

--- backend/tourify/settings.py
+++ backend/tourify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%5djo6*d90dxi=f)%n-qbwhe!&t9b2kd5+j(2ys&f8k67=s3--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://tourify-backend.onrender.com/']
 
