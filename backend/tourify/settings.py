63a72743a3c6a19e7590f7f39cff63d96f61ce8b
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-21 16:27:37 +0100

    Add URL to ALLOWED_HOSTS

--- backend/tourify/settings.py
+++ backend/tourify/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%5djo6*d90dxi=f)%n-qbwhe!&t9b2kd5+j(2ys&f8k67=s3--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://tourify-backend.onrender.com/']
 
 
 # Application definition
